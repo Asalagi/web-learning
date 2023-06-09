042d9c19030d00b0fe69754470b6fe01e201f5a4
Asalagi <mypaintedfrog@yahoo.com>
2023-06-09 10:13:41 -0500

    dog age to humane age snip

---
+++ JavaScript Learning/dog-age.js
@@ -0,0 +1,4 @@
+var dogName = prompt("What is your dog's name?");
+var dogAge = prompt("What is your dog's age?");
+var dogToHuman = ((dogAge - 2) * 4) + 21;
+alert(dogName + " is " + dogToHuman + " years old in human years.");
