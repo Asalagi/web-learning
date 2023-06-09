27ae1fbee77d44ae979a2cfaf9d286032b6876d1
Asalagi <mypaintedfrog@yahoo.com>
2023-06-09 15:13:48 -0500

    function cost and change

--- JavaScript Learning/functions.js
+++ JavaScript Learning/functions.js
@@ -24,3 +24,23 @@ function getGrain(money) {
   }
 
   getGrain(100);
+
+
+  // spiffed up grain and change function
+
+  function getGrain(money) {   
+    console.log("buy " + buyGrain(money, 21.99) + " bags of grain.");
+    console.log("you have " + grainChange(money, 21.99) + " left in your budget.");
+  }
+
+function buyGrain(myMoney, grainCost) {
+    var bags = Math.floor(myMoney / grainCost);
+    return bags;
+}
+
+function grainChange(myMoney, grainCost) {
+    var change = myMoney % grainCost;
+    return change;
+}
+
+getGrain(100);
