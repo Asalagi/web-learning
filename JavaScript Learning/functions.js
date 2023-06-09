307cc7d6904481a0f315ebd176c9820d2bd710fd
Asalagi <mypaintedfrog@yahoo.com>
2023-06-09 11:51:58 -0500

    functions and making robot buy grain bags

---
+++ JavaScript Learning/functions.js
@@ -0,0 +1,26 @@
+function getGrain(money) {   
+    console.log("leaveHouse");
+    console.log("moveRight");
+    console.log("moveRight");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveRight");
+    console.log("moveRight");
+
+    var bags = Math.floor(money / 24.99);
+    
+    console.log("buy " + bags + " bags of grain")
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("enterHouse");
+  }
+
+  getGrain(100);
