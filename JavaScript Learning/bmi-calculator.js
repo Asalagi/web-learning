b51a0afa898583771d8ffaa76be91c51c60be659
Asalagi <mypaintedfrog@yahoo.com>
2023-06-12 15:48:19 -0500

    bmi if else update

--- JavaScript Learning/bmi-calculator.js
+++ JavaScript Learning/bmi-calculator.js
@@ -28,6 +28,23 @@ var bmi = bmiCalculator(223, 64);
 bmi;
 
 
+//fourth try with if else statement
+function bmiCalculator(weight, height) {
+    var bmi = Math.round(weight / (height * height));
+    if (bmi < 18.5) {
+        alert("Your BMI is " + bmi + ", you are under weight. Eat a cheese burger!");
+    } else if (bmi >= 18.6 && bmi <= 24.9) {
+       alert("Your BMI is " + bmi + ", you are at an ideal weight. Keep it up beach bod!");
+    } else if (bmi > 24.9) {
+        alert("Your BMI is + " + bmi + ", you are a fat. You need to lose weight you chunky monkey.");
+    }
+
+    return bmi;
+}
+
+var bmiResult = bmiCalculator(75, 2);
+bmiResult;
+
 // var weight = prompt("What is your weight in pounds?");
 // var height = prompt("What is your height in inches?");
 // var total = weight / (height * height);
