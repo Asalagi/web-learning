01fbef3a5feba8cf8fdf19d5bd43ff69c37ee422
Asalagi <mypaintedfrog@yahoo.com>
2023-06-12 14:12:59 -0500

    updating love score with if else

--- JavaScript Learning/love-score-project.js
+++ JavaScript Learning/love-score-project.js
@@ -4,3 +4,17 @@ prompt("What is their name?");
 var love = Math.random() * 100;
 love = Math.floor(love) + 1;
 alert("You are a " + love + "% match!");
+
+// using if else
+
+prompt("What is your name?");
+prompt("What is their name?");
+
+var love = Math.random() * 100;
+love = Math.floor(love) + 1;
+
+if (love > 70){ 
+   alert("You got a " + love + "% score! Your future looks to be bright and happy!");
+} else {
+    alert("You got a " + love + "% score. Maybe you should seek out other options. Perhaps a cat.");
+}
