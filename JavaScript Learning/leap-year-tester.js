afae10ffbb5465d288301c5b01a129f903dd72b9
Asalagi <mypaintedfrog@yahoo.com>
2023-06-13 11:19:16 -0500

    leap year update

--- JavaScript Learning/leap-year-tester.js
+++ JavaScript Learning/leap-year-tester.js
@@ -1,15 +1,26 @@
 function isLeap(year) {
   
     if (year % 4 === 0) {
-        console.log("Leap year.")
+        console.log("Leap year.");
         } else if (year % 100 !== 0) {
-        console.log("Not leap year.")
+        console.log("Not leap year.");
         } else if (year % 400 === 0) {
-        conosle.log("Leap year.")
-        }
+        console.log("Leap year.");
+        } 
      return year;
       
-    
     }
     
     isLeap(2024);
+
+// corrected and with short hand
+
+function isLeap(year) {
+    if (year % 4 === 0 && year % 100 === 0 && year % 400 === 0 || year % 4 === 0 && year % 100 !== 0) {
+        return "leap year."
+    } else {
+        return "not a leap year."
+    }
+}
+
+isLeap(1700);
