852ed0e76041861cb5079f2d06505fb1f54d87e4
Asalagi <mypaintedfrog@yahoo.com>
2023-06-13 09:41:00 -0500

    making leap year tester

---
+++ JavaScript Learning/leap-year-tester.js
@@ -0,0 +1,15 @@
+function isLeap(year) {
+  
+    if (year % 4 === 0) {
+        console.log("Leap year.")
+        } else if (year % 100 !== 0) {
+        console.log("Not leap year.")
+        } else if (year % 400 === 0) {
+        conosle.log("Leap year.")
+        }
+     return year;
+      
+    
+    }
+    
+    isLeap(2024);
