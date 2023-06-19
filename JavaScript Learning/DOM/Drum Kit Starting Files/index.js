e1fbfb9a006740cbd4d37ae985017b2c13d5b9b6
Asalagi <mypaintedfrog@yahoo.com>
2023-06-19 10:56:34 -0500

    adding background and sound to drums

--- JavaScript Learning/DOM/Drum Kit Starting Files/index.js
+++ JavaScript Learning/DOM/Drum Kit Starting Files/index.js
@@ -3,6 +3,8 @@ var drumDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < drumDrumButtons; i++) {
     document.querySelectorAll("button")[i].addEventListener("click", function handleClick() {
-        alert("I got clicked");
+        var audio = new Audio('sounds/tom-1.mp3');
+        audio.play();
     });
 }
+
