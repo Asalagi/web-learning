3b5f0c6cc33054e2a0120d9db7750fe257e4db4c
Asalagi <mypaintedfrog@yahoo.com>
2023-06-26 11:06:37 -0500

    name generator with node

--- JavaScript Learning/2.3 NPM/index.js
+++ JavaScript Learning/2.3 NPM/index.js
@@ -1,11 +1,4 @@
-const fs = require("fs");
+var generateName = require("sillyname");
+var sillyName = generateName();
 
-// fs.writeFile("message.txt", "Hello from Node.js!", (err) => {
-//     if (err) throw err;
-//     console.log("The files has been saved!");
-// });
-
-fs.readFile("./message.txt", "utf8", (err, data) => {
-    if (err) throw err;
-    console.log(data);
-  }); 
+console.log(`My name is ${sillyName}.`);
