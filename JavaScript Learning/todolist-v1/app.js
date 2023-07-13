2883cc6d49c0ec7566ea1daad6454b25497f2ea9
Asalagi <mypaintedfrog@yahoo.com>
2023-07-13 16:09:01 -0500

    todo list with ejs

--- JavaScript Learning/todolist-v1/app.js
+++ JavaScript Learning/todolist-v1/app.js
@@ -4,39 +4,30 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
+var items = [];
+
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
     var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
 
-    switch (currentDay) {
-        case 0:
-            day = "Sunnudagr";
-            break;
-        case 1:
-            day = "Manadagr";
-            break;
-        case 2:
-            day = "Tysdagr";
-             break;
-        case 3:
-            day = "Odinsdagr";
-            break;
-        case 4:
-            day = "Thorsdagr";
-            break;
-        case 5:
-            day = "Frjadagr";
-            break;
-        case 6: 
-            day = "Laugardagr";
-            break;
-            default:
-                console.log("Error today is " + currentDay);
-    }
-    res.render("list", {kindOfDay: day})
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    var day = today.toLocaleDateString("en-US", options);
+
+    res.render("list", {kindOfDay: day, newListItems: items})
+});
+
+app.post("/", function(req, res){
+    var item = req.body.newItem;
+    items.push(item);
+    res.redirect("/");
 });
 
 app.listen(3000, function(){
