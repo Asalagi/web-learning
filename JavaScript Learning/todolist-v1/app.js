f13462bd7448909f7802f4815c849b0e072ec191
Asalagi <mypaintedfrog@yahoo.com>
2023-07-14 09:31:08 -0500

    add style

--- JavaScript Learning/todolist-v1/app.js
+++ JavaScript Learning/todolist-v1/app.js
@@ -6,26 +6,26 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-var items = [];
+let items = [];
 
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
-    var today = new Date();
+    let today = new Date();
 
-    var options = {
+    let options = {
         weekday: "long",
         day: "numeric",
         month: "long"
     };
 
-    var day = today.toLocaleDateString("en-US", options);
+    let day = today.toLocaleDateString("en-US", options);
 
     res.render("list", {kindOfDay: day, newListItems: items})
 });
 
 app.post("/", function(req, res){
-    var item = req.body.newItem;
+    let item = req.body.newItem;
     items.push(item);
     res.redirect("/");
 });
