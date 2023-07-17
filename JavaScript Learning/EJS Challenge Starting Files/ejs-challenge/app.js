a516518a136a8a7cb960c59010a42d58a9f895f3
Asalagi <mypaintedfrog@yahoo.com>
2023-07-17 14:11:56 -0500

    add compose page

--- JavaScript Learning/EJS Challenge Starting Files/ejs-challenge/app.js
+++ JavaScript Learning/EJS Challenge Starting Files/ejs-challenge/app.js
@@ -20,11 +20,15 @@ app.get("/", function(req, res){
 });
 
 app.get("/about", function(req, res){
-  res.render("about", {about: aboutContent});
+  res.render("about", {aboutContent: aboutContent});
 });
 
 app.get("/contact", function(req, res){
-  res.render("contact", {contact: contactContent});
+  res.render("contact", {contactContent: contactContent});
+});
+
+app.get("/compose", function(req, res){
+  res.render("compose");
 });
 
 
