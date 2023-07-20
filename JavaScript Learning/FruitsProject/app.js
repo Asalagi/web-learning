78156553a7b514d952d62df8a590b9f5b5d11549
Asalagi <mypaintedfrog@yahoo.com>
2023-07-20 11:13:05 -0500

    update mongodb driver

--- JavaScript Learning/FruitsProject/app.js
+++ JavaScript Learning/FruitsProject/app.js
@@ -1,18 +1,26 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+const uri = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.10.1";
 
-const url = 'mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.10.1';
+const client = new MongoClient(uri,  {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    }
+);
 
-const dbName = 'fruitsDB';
+async function run() {
+  try {
 
-const client = new MongoClient(url, { useNewUrlParser: true });
+    await client.connect();
 
-client.connect(function(err) {
-    assert.equal(null, err);
-    console.log("Connected successfully to server");
+    await client.db("admin").command({ ping: 1 });
+    console.log("You successfully connected to MongoDB!");
+  } finally {
 
-    const db = client.db(dbName);
-
-    client.close();
-});
+    await client.close();
+  }
+}
+run().catch(console.dir);
