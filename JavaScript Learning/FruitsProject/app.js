29e762931924609c940763de67cc4a8ee4a728ab
Asalagi <mypaintedfrog@yahoo.com>
2023-07-21 10:06:25 -0500

    attempt to add to mongoDB

--- JavaScript Learning/FruitsProject/app.js
+++ JavaScript Learning/FruitsProject/app.js
@@ -1,4 +1,5 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
+const assert = require("assert");
 
 const uri = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.10.1";
 
@@ -12,15 +13,48 @@ const client = new MongoClient(uri,  {
 );
 
 async function run() {
+  let db;
+
   try {
 
     await client.connect();
 
-    await client.db("admin").command({ ping: 1 });
+    db = client.db("fruitsDB").command({ ping: 1 });
     console.log("You successfully connected to MongoDB!");
   } finally {
 
-    await client.close();
+    insertDocuments(db, function(){
+      client.close();
+    });
   }
 }
+const insertDocuments = function(db, callback) {
+  const collection = db.collection('fruits');
+
+  collection.insertMany([
+      {
+          name: "Apple",
+          score: 8,
+          review: "Apple a day keeps the doctor away"
+      },
+      {
+          name: "Orange",
+          score: 1,
+          review: "Yuck"
+      },
+      {
+          name: "Banana",
+          score: 5,
+          review: "Keeps my potassium up"
+      }
+  ], function(err, result) {
+      assert.equal(err, null);
+      assert.equal(3, result.insertedCount);
+      assert.equal(3, result.ops.length);
+      console.log("Inserted 3 documents into the collection");
+      callback(result);
+  });
+};
+
 run().catch(console.dir);
+
