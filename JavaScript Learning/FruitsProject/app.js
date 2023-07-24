f56bdb14a111a60d5e088c755da0f8a85cf48123
Asalagi <mypaintedfrog@yahoo.com>
2023-07-24 12:27:09 -0500

    people and fruits

--- JavaScript Learning/FruitsProject/app.js
+++ JavaScript Learning/FruitsProject/app.js
@@ -1,25 +1,59 @@
 
+const { MongoDBCollectionNamespace } = require('mongodb');
 const mongoose = require('mongoose');
 
 // const uri = 'mongodb://127.0.0.1:27017/fruitsDB';
 
 mongoose.connect("mongodb://127.0.0.1:27017/fruitsDB");
 
-  const fruitSchema = new mongoose.Schema({
-    name: String,
-    score: Number,
-    review: String
-  });
+  // const fruitSchema = new mongoose.Schema({
+  //   name: String,
+  //   score: Number,
+  //   review: String
+  // });
   
-  const Fruit = mongoose.model('Fruit', fruitSchema);
+  // const Fruit = mongoose.model('Fruit', fruitSchema);
+
+  // const fruit = new Fruit ({
+  //   name: "Pear",
+  //   score: 8,
+  //   review: "sweet!"
+  // });
 
   const fruit = new Fruit ({
-    name: "Apple",
-    score: 7,
-    review: "Crunchy and sweet!"
+    name: "Kiwi",
+    score: 1,
+    review: "too fuzzy"
   });
+
+  const fruit = new Fruit ({
+    name: "Banana",
+    score: 5,
+    review: "Good when there is nothing else to eat"
+  });
+
+  const fruit = new Fruit ({
+    name: "Strawberry",
+    score: 10,
+    review: "so sweet and delicious"
+  });
+
+  // const personSchema = new mongoose.Schema({
+  //   name: String,
+  //   age: Number,
+  //   state: String
+  // });
   
-  fruit.save();
-  console.log('Inserted 1 document into the collection');
-  mongoose.connection.close();
-  process.exit(0);
+  // const Person = mongoose.model('Person', personSchema);
+  // const person = new Person ({
+  //   name: "Jerry",
+  //   age: "32",
+  //   state: "Oklahoma"
+  // });
+
+  // person.save();
+
+  Fruit.insertMany();
+  console.log("many fruits were inserted");
+  // fruit.save();
+  // console.log('Inserted 1 document into the collection');
