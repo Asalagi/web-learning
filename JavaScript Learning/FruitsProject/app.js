87ebf2decd4e8eb26288032107c4535e897bd4af
Asalagi <mypaintedfrog@yahoo.com>
2023-07-24 11:34:05 -0500

    add fruit db with mongo and mongoose

--- JavaScript Learning/FruitsProject/app.js
+++ JavaScript Learning/FruitsProject/app.js
@@ -1,70 +1,25 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const assert = require("assert");
 
-const uri =
-  "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.10.1";
+const mongoose = require('mongoose');
 
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
+// const uri = 'mongodb://127.0.0.1:27017/fruitsDB';
 
-async function run() {
-  try {
-    await client.connect();
-    console.log("You successfully connected to MongoDB!");
+mongoose.connect("mongodb://127.0.0.1:27017/fruitsDB");
 
-    const db = client.db("fruitsDB");
+  const fruitSchema = new mongoose.Schema({
+    name: String,
+    score: Number,
+    review: String
+  });
+  
+  const Fruit = mongoose.model('Fruit', fruitSchema);
 
-    await db.command({ ping: 1 });
-
-    // Call the insertDocuments function and use async/await to ensure it finishes before closing the client
-    await insertDocuments(db);
-
-  } catch (err) {
-    console.error("Error connecting to MongoDB:", err);
-  } finally {
-    client.close();
-    console.log("Connection to MongoDB closed.");
-  }
-}
-
-const insertDocuments = async function (db) {
-  const collection = db.collection("fruits");
-
-  try {
-    const result = await collection.insertMany([
-      {
-        name: "Apple",
-        score: 8,
-        review: "Apple a day keeps the doctor away",
-      },
-      {
-        name: "Orange",
-        score: 1,
-        review: "Yuck",
-      },
-      {
-        name: "Banana",
-        score: 5,
-        review: "Keeps my potassium up",
-      },
-    ]);
-
-    assert.equal(3, result.insertedCount);
-    
-    if (result.ops) {
-      assert.equal(3, result.ops.length);
-      console.log("Inserted 3 documents into the collection");
-    } else {
-      console.log("No documents were inserted.");
-    }
-  } catch (err) {
-    console.error("Error inserting documents:", err);
-  }
-};
-
-run().catch(console.dir);
+  const fruit = new Fruit ({
+    name: "Apple",
+    score: 7,
+    review: "Crunchy and sweet!"
+  });
+  
+  fruit.save();
+  console.log('Inserted 1 document into the collection');
+  mongoose.connection.close();
+  process.exit(0);
