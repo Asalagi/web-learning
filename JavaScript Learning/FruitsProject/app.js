32ef0fb3ae9cb9b35bafea4d1497317fb96391bd
Asalagi <mypaintedfrog@yahoo.com>
2023-07-24 13:27:27 -0500

    find fruits and close connection

--- JavaScript Learning/FruitsProject/app.js
+++ JavaScript Learning/FruitsProject/app.js
@@ -4,7 +4,15 @@ const mongoose = require('mongoose');
 
 // const uri = 'mongodb://127.0.0.1:27017/fruitsDB';
 
-mongoose.connect("mongodb://127.0.0.1:27017/fruitsDB");
+mongoose.connect("mongodb://127.0.0.1:27017/fruitsDB")
+.then(() => {
+  console.log('You successfully connected to MongoDB using Mongoose!');
+  findFruits();
+})
+.catch(err => {
+  console.error('Error connecting to MongoDB using Mongoose:', err);
+  process.exit(1); // Exit the process with a non-zero exit code to indicate an error
+});
 
   const fruitSchema = new mongoose.Schema({
     name: String,
@@ -14,30 +22,13 @@ mongoose.connect("mongodb://127.0.0.1:27017/fruitsDB");
   
   const Fruit = mongoose.model('Fruit', fruitSchema);
 
-  // const pear = new Fruit ({
-  //   name: "Pear",
-  //   score: 8,
-  //   review: "sweet!"
-  // });
-
-  const kiwi = new Fruit ({
-    name: "Kiwi",
-    score: 1,
-    review: "too fuzzy"
-  });
-
-  const banana = new Fruit ({
-    name: "Banana",
-    score: 5,
-    review: "Good when there is nothing else to eat"
-  });
-
-  const strawberry = new Fruit ({
-    name: "Strawberry",
-    score: 10,
-    review: "so sweet and delicious"
-  });
-
+  const fruits = [
+    { name: 'Pear', score: 8, review: "sweet!"},
+    { name: 'Kiwi', score: 1, review: 'Too fuzzy' },
+    { name: 'Banana', score: 5, review: 'Good when there is nothing else to eat' },
+    { name: 'Strawberry', score: 10, review: 'So sweet and delicious' }
+  ];
+ 
   // const personSchema = new mongoose.Schema({
   //   name: String,
   //   age: Number,
@@ -53,7 +44,20 @@ mongoose.connect("mongodb://127.0.0.1:27017/fruitsDB");
 
   // person.save();
 
-  Fruit.insertMany([kiwi, banana, strawberry]);
-  console.log("inserted many fruits");
+  // Fruit.insertMany(fruits);
+  // console.log("inserted many fruits");
   // fruit.save();
   // console.log('Inserted 1 document into the collection');
+  async function findFruits() {
+    try {
+      const fruits = await Fruit.find().select('name');
+      console.log('Found fruits:');
+      fruits.forEach(fruit => console.log(fruit.name));
+    } catch (err) {
+      console.error('Error finding fruits:', err);
+    } finally {
+      mongoose.connection.close();
+      console.log('Connection to MongoDB closed.');
+      process.exit(0); // Exit the process with a zero exit code to indicate success
+    }
+  }
